ac1bfd1d98d99ec44dbc83ee037d81b210acab6e
Myra XIE <2213926@mail.nankai.edu.cn>
2024-03-28 09:42:58 +0000

    src/observer/sql/parser/value.h

--- src/observer/sql/parser/value.h
+++ src/observer/sql/parser/value.h
@@ -27,6 +27,7 @@ enum AttrType
   INTS,      ///< 整数类型(4字节)
   FLOATS,    ///< 浮点数类型(4字节)
   BOOLEANS,  ///< boolean类型，当前不是由parser解析出来的，是程序内部使用的
+  DATES,
 };
 
 const char *attr_type_to_string(AttrType type);
@@ -47,6 +48,7 @@ public:
   explicit Value(float val);
   explicit Value(bool val);
   explicit Value(const char *s, int len = 0);
+  explicit Value(const char *date, int len,int flag);
 
   Value(const Value &other)            = default;
   Value &operator=(const Value &other) = default;
@@ -55,6 +57,7 @@ public:
   void set_data(char *data, int length);
   void set_data(const char *data, int length) { this->set_data(const_cast<char *>(data), length); }
   void set_int(int val);
+  void set_date(int val);
   void set_float(float val);
   void set_boolean(bool val);
   void set_string(const char *s, int len = 0);
@@ -75,9 +78,11 @@ public:
    * 如果当前的类型与期望获取的类型不符，就会执行转换操作
    */
   int         get_int() const;
+  int         get_date() const;
   float       get_float() const;
   std::string get_string() const;
   bool        get_boolean() const;
+
 
 private:
   AttrType attr_type_ = UNDEFINED;
@@ -88,6 +93,10 @@ private:
     int   int_value_;
     float float_value_;
     bool  bool_value_;
+    int   date_value_;
   } num_value_;
   std::string str_value_;
 };
+std::string intDate_to_strDate(int int_date);
+int value_init_date(const char* v);
+bool check_date(int y, int m, int d);
